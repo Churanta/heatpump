8864b80f154eb400e107d0016817b92a67074c65
churanta <churantamondal321@gmail.com>
2022-12-25 15:50:25 +0530

    updated version

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ app.config['MYSQL_DATABASE_DB'] = 'heat_pump'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-
 # this will be the login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -43,26 +42,19 @@ def login():
         password = request.form['password']
         # Check if account exists using MySQL
         cursor.execute(
-            'SELECT * FROM tbl_admin WHERE name = %s OR email = %s ', (name, name))
+            'SELECT * FROM tbl_admin WHERE name = %s OR email = %s AND password = %s', (name, name, password))
         # Fetch one record and return result
         account = cursor.fetchone()
 
     # If account exists in accounts table in out database
         if account:
             # Create session data, we can access this data in other routes
-            session['db_id'] = account['id']
-            session['db_name'] = account['name']
-            session['db_pass'] = account['password']
-            session['db_mail'] = account['email']
-            print("////////////////////////////////////////////////////")
-            print(session['db_id'])
-            if session['db_mail'] == name or session['db_name'] == name:
-                if session['db_pass'] == password:
-                    session['loggedin'] = True
-                    session['name'] = account['name']
-                    session['password'] = account['password']
-                    session['email'] = account['email']
-                return redirect(url_for('home'))
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['name'] = account['name']
+            session['password'] = account['password']
+            session['email'] = account['email']
+            return redirect(url_for('home'))
         else:
             # Account doesnt exist or name/password incorrect
             return redirect(url_for('login'))
@@ -70,8 +62,6 @@ def login():
     return render_template('login.html', msg=msg)
 
 # this will be the registration page
-
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
  # connect
@@ -89,25 +79,18 @@ def register():
 
     # Check if account exists using MySQL
         cursor.execute(
-            'SELECT * FROM tbl_admin WHERE email = %s', (email))
+            'SELECT * FROM tbl_admin WHERE name = %s', (name))
         account = cursor.fetchone()
         # If account exists show error and validation checks
         if account:
             msg = 'Account already exists!'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address!'
-        elif not re.match(r'[A-Za-z0-9]+', name):
-            msg = 'name must contain only characters and numbers!'
-        elif not name or not password or not email:
-            msg = 'Please fill out the form!'
         else:
             print("everything is perfect before random")
 
             uid_new = random_uid(1)
             NULL = ""
             # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO tbl_admin VALUES (%s, %s, %s,%s,%s,%s,%s,%s)',
-                            (uid_new, name, email, password, NULL, NULL, NULL, 0))
+            cursor.execute('INSERT INTO tbl_admin VALUES (%s, %s, %s, %s, %s, %s, %s, %s)', (uid_new, name, email, password, NULL, NULL, NULL, 0))
             conn.commit()
             return render_template('login.html', msg=msg)
 
@@ -124,14 +107,6 @@ def home():
     if 'loggedin' in session:
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        print("////////////////////////////////////////////////////")
-        print(session['db_id'])
-        cursor.execute('Select * from tbl_admin where id = %s',(session.get('db_id')))
-        details = cursor.fetchone()
-        conn.commit()
-        name = details['name']
-        print("////////////////////////////////////////////////////")
-        print(details)
         if request.method == 'POST' and 'rcp_temp' in request.form and 'rcp_humidity' in request.form and 'rcp_cmt' in request.form and 'rcp_name' in request.form:
             # Create variables for easy access
             rcp_temp = request.form['rcp_temp']
@@ -139,10 +114,10 @@ def home():
             rcp_cmt = request.form['rcp_cmt']
             rcp_name = request.form['rcp_name']
             rcp_id = random_rcp_uid(2)
-            print("////////////////////////////////////////////////////")
+            rcp_uid = session.get('id')
             # Account doesnt exists and the form data is valid, now insert new account into accounts table
             cursor.execute('INSERT INTO tbl_recipe VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)',
-                                (rcp_id,rcp_name,'Wjfnjaj372508599',rcp_temp,rcp_humidity,rcp_cmt,0,0,0))
+                                (rcp_uid, rcp_id, rcp_name , rcp_temp, rcp_humidity, rcp_cmt, 0, 0, 0))
             conn.commit()
             return redirect(url_for('list'))
          # User is loggedin show them the home page
@@ -155,7 +130,21 @@ def home():
 def home2():
     # Check if user is loggedin
     if 'loggedin' in session:
-
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        if request.method == 'POST' and 'rcp_temp' in request.form and 'rcp_humidity' in request.form and 'rcp_cmt' in request.form and 'rcp_name' in request.form:
+            # Create variables for easy access
+            rcp_temp = request.form['rcp_temp']
+            rcp_humidity = request.form['rcp_humidity']
+            rcp_cmt = request.form['rcp_cmt']
+            rcp_name = request.form['rcp_name']
+            rcp_id = random_rcp_uid(2)
+            rcp_uid = session.get('id')
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO tbl_recipe VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                                (rcp_uid, rcp_id, rcp_name, rcp_temp, rcp_humidity, rcp_cmt, 0, 0, 0))
+            conn.commit()
+            return redirect(url_for('list'))
          # User is loggedin show them the home page
         return render_template('index.html', name=session.get('name'))
     # User is not loggedin redirect to login page
@@ -173,128 +162,88 @@ def logout():
     # Redirect to login page
     return redirect(url_for('login'))
 
-
 @app.route('/list', methods=['POST', 'GET'])
 def list():
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        rcp_uid = session.get('rcp_uid')
-        
-        cursor.execute('SELECT * FROM `tbl_recipe` WHERE rcp_uid=%s',("Wjfnjaj372508599") )
-        account = cursor.fetchone()
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    rcp_uid = session.get('id')
+    Notification_list=[]
+    def fetch_list() -> dict:
+        cursor.execute('SELECT * FROM `tbl_recipe` WHERE rcp_uid=%s',(rcp_uid))
+        recipe_list = cursor.fetchall()
         conn.commit()
         conn.close()
-        rcp_uid= account['rcp_uid']
-        Notification_list=[]
-        def fetch_list() -> dict:
-            conn = mysql.connect()
-            cursor = conn.cursor(pymysql.cursors.DictCursor)
-            rcp_uid = session.get('rcp_uid')
-            cursor.execute('SELECT * FROM `tbl_recipe` WHERE rcp_uid=%s',("Wjfnjaj372508599"))
-            account= cursor.fetchall()
-            conn.commit()
-            conn.close()
-            for k in account:
-                lists = {
-                    "rcp_name": k['rcp_name'],
-                    "rcp_temp": k['rcp_temp'],
-                    "rcp_humidity": k['rcp_humidity'],
-                    "rcp_ctime": k['rcp_ctime']
-                }
-                Notification_list.append(k)
-            return Notification_list
-        session['list'] = fetch_list()
-        session['rcp_uid'] = rcp_uid      
-        return render_template('/list.html',name=session.get('name'),items= session['list'],rcp_uid=rcp_uid )
+        for results in recipe_list:
+            lists = {
+                "rcp_name": results['rcp_name'],
+                "rcp_temp": results['rcp_temp'],
+                "rcp_humidity":results['rcp_humidity'],
+                "rcp_ctime": results['rcp_ctime']
+            }
+            Notification_list.append(lists)
+        return Notification_list
+    session['list'] = fetch_list()     
+    return render_template('/list.html',name=session.get('name'),items= session['list'])
 
-@app.route('/device')
+@app.route('/device', methods=['POST', 'GET'])
 def device():
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
-    UserId = session.get('UserId')
-    
-    cursor.execute('SELECT * FROM `tbl_devices` WHERE UserId=%s',("Wjfnjaj372508599"))
+    UserId = session.get('id')
+    cursor.execute('SELECT * FROM `tbl_devices` WHERE `id`=%s',(UserId))
      # Fetch one record and return result
-    account = cursor.fetchone()
+    device = cursor.fetchone()
     conn.commit()
-    conn.close()
-    UserId = account['device_count']
-    Notification_list = []
+    device_count = device['device_count']
+    device_list = []
     def fetch_list() -> dict:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        UserId = session.get('UserId')
-        cursor.execute('SELECT * FROM `tbl_devices` WHERE UserId=%s',("Wjfnjaj372508599"))
-        account = cursor.fetchall()
+        cursor.execute('SELECT * FROM `tbl_devices` WHERE `id`=%s',(UserId))
+        devices = cursor.fetchall()
         conn.commit()
-        conn.close()
-        for j in account:
-            lists ={
+        for j in devices:
+            lists = {
                 "device_id": j['device_id'],
-                "UserId": j['UserId'],
-                "device_nm":j['device_nm'],
-                "time_left":j['time_left']
+                "id": j['id'],
+                "device_nm": j['device_nm'],
+                "time_left": j['time_left']
             }
-            Notification_list.append(j)
-        return Notification_list
-    session['list'] =fetch_list()
-    session['UserId'] = UserId
-    
-    return render_template('/device.html',name=session.get('name'), items= session['list'],UserId=UserId )
+            device_list.append(lists)
+        return device_list
+    session['device_list'] = fetch_list()
+    return render_template('/device.html', name=session.get('name'), devices=session['device_list'], device_count=device_count)
 
-@app.route('/Notifications',)
+@app.route('/Notifications', methods=['POST', 'GET'])
 def Notifications():
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
-    uid = session.get('uid')
-    
-    cursor.execute('SELECT * FROM tbl_notifuy WHERE uid=%s',("Wjfnjaj372508599"))
-    
-        # Fetch one record and return result
-    account = cursor.fetchone()
+    id = session.get('id') 
+    cursor.execute('SELECT * FROM `tbl_notifi` WHERE `id`=%s',(id))
+    # Fetch one record and return result
+    notifi = cursor.fetchone()
     conn.commit()
-    conn.close()
-    total_list = account['not_list_total']
+    total_list = notifi['not_list_total']
     Notification_list = []
     def fetch_list() -> dict:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        uid = session.get('uid')
-        cursor.execute('SELECT * FROM tbl_notifuy WHERE uid=%s',("Wjfnjaj372508599"))
-        account = cursor.fetchall()
+        cursor.execute('SELECT * FROM `tbl_notifi` WHERE `id`=%s',(id))
+        notifi_list = cursor.fetchall()
         conn.commit()
-        conn.close()
-        for i in account:
+        for result in notifi_list:
             lists ={
-                "Dev_id": i['Dev_id'],
-                "not_type": i['not_type'],
-                "not_comment":i['not_comment'],
-                "time":i['not_time']
+                "Dev_id": result['Dev_id'],
+                "not_type": result['not_type'],
+                "not_comment": result['not_comment'],
+                "time": result['not_time']
             }
-            Notification_list.append(i)
+            Notification_list.append(lists)
         return Notification_list
-    session['list'] =fetch_list()
-    session['total_list'] = total_list
-    
-    return render_template('/Notifications.html', items = session['list'], total_lists=total_list,name=session.get('name') )
-    # if account:
-    #     session['uid'] = account['uid']
-    #     session['dev_id'] = account['Dev_id']
-    #     session['not_type'] = account['not_type']
-    #     session['noti_list'] = account['not_list_total']
-    #     session['not_comment'] = account['not_comment']
-    #     session['not_time'] = account['not_time']
-    #     print("///////////////////////////////////////////////////////////")
-    #     print(session['noti_list'])
-    #     print("///////////////////////////////////////////////////////////")
-    #     return render_template('/Notifications.html',name=session.get('name'),uid=session.get('uid'),Dev_id=session.get('dev_id'),not_type=session.get('not_type'),not_comment=session.get('not_comment'),not_time=session.get('not_time'),list_count =account['not_list_total'])
-    # return redirect(url_for('home'))
+    session['notifi_list'] = fetch_list()
+    print("///////////////////////////////")
+    print(session['notifi_list'])
+    return render_template('/Notifications.html', name=session.get('name'), notifications = session['notifi_list'], total_lists=total_list)
     
 @app.route('/Help',)
 def Help():    
-    return render_template('/Help.html',name=session.get('name') )
-    
-    
+    return render_template('/Help.html',name=session.get('name') ) 
 
 @app.route('/auth')
 def api():
@@ -312,8 +261,8 @@ def api():
         account = cursor.fetchone()
         if account:
             db_email= account['email']
-            db_password=account['password']
-            if db_email == user and db_password == pswd:
+            passwordword=account['password']
+            if db_email == user and passwordword == pswd:
                 access = 1
                 chart= {"Response":200,
                 "name": account['name'],
@@ -322,7 +271,7 @@ def api():
                 "logo": account['logo'],
                 "time": account['created_time'],
                 "access":access}
-            elif db_email != user or db_password != pswd:
+            elif db_email != user or passwordword != pswd:
                 chart= {"Response":400,
                 "API":"False",
                 "name": user,
